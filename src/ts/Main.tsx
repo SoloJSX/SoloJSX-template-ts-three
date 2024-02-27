12bc1f69961b9dea74edba95d3c3a3ccb6295db5
ee <e2gamedev@outlook.com>
2024-02-27 15:44:49 +0100

    updated onclick

--- src/ts/Main.tsx
+++ src/ts/Main.tsx
@@ -1,15 +1,16 @@
 import solojsx from 'solojsx';
 export default class Main {
     static init() {
-        const e = (
+        const onClick = (text: string) => alert(text);
+        const element = (
             <div>
                 <h1>Vite + TypeScript + SoloJSX</h1>
                 <p>Minimal example</p>
                 <br />
-                <div style="cursor: pointer" onclick={() => alert("Hello")}>Click me</div>
+                <div style="cursor: pointer" onclick={() => onClick("Hello world")}>Click me</div>
                 <br />
                 <img src="/vite.svg" alt="" />
-            </div>);
-        document.body.append(e);
+            </div >);
+        document.body.append(element);
     }
 }
