af9c21c798f126846a3eaee0db10dfee149435a6
eekelof <u@w>
2024-08-26 02:00:27 +0200

    cleanup

--- src/ts/Main.tsx
+++ src/ts/Main.tsx
@@ -1,6 +1,6 @@
 export default class Main {
     static init() {
-        const onClick = (text: string) => alert(text);
+        const onClick = (text: string) => alert(text)
         const element = (
             <div>
                 <h1>Vite + TypeScript + SoloJSX</h1>
@@ -9,7 +9,7 @@ export default class Main {
                 <div style="cursor: pointer" onclick={() => onClick("Hello world22")}>Click me</div>
                 <br />
                 <img src="/vite.svg" alt="" />
-            </div >);
-        document.body.append(element);
+            </div >)
+        document.body.append(element)
     }
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,2 @@
-import Main from "./Main";
-Main.init();
+import Main from "./Main"
+Main.init()
